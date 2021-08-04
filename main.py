7eb21665cb78b1f632c70fc93e9074a531b77f58
Renato Lewkowicz <renato.lew@gmail.com>
2021-08-04 14:26:07 -0300

    First commit, creating README file and setting up Django enviroment.

---
+++ main.py
@@ -0,0 +1,6 @@
+# To Do list:
+# Set up Django
+# Create a model to be managed by Django ORM
+# Set up Django REST Framework
+# Serialize the previously created model
+# Create the URI endpoints to view the serialized data
